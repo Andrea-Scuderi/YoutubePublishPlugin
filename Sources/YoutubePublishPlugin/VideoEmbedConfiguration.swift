49d7aa419dc8cafdfecae95072be852352867037
Andrea Scuderi <andrea.scuderi@ymail.com>
2024-10-06 08:44:09 +0200

    Add VideoEmbedConfiguration public init

--- Sources/YoutubePublishPlugin/VideoEmbedConfiguration.swift
+++ Sources/YoutubePublishPlugin/VideoEmbedConfiguration.swift
@@ -9,11 +9,12 @@ import Foundation
 public struct VideoEmbedConfiguration  {
     var width: Int?
     var height: Int?
-    var className: String = "embeddedVideo"
+    var className: String
     
-    init(width: Int = 560, height: Int = 315) {
+    public init(width: Int = 560, height: Int = 315, className: String = "embeddedVideo") {
         self.width = width
         self.height = height
+        self.className = className
     }
     
     public static var `default`: VideoEmbedConfiguration { VideoEmbedConfiguration() }
